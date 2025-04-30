ca10772f098f696c61c560be5b9fcc123597b04a
Chad Anderson <Chad.Anderson@vu.com>
2025-04-30 16:45:01 -0500

    searched file for From lines, sliced third word, added to dictionary, if already in dictionary increased value by 1

--- counter.py
+++ counter.py
@@ -1,7 +1,23 @@
 def countDayOfTheWeek():
     # This first line is provided for you
+    counter = dict()
     file_name = input("Enter a file name: ")
+    try: 
+        fhandle = open(file_name)
+    except:
+        print('The Princess is in another castle!')
+        exit()
+    for line in fhandle:
+        line = line.rstrip()
+        if not line.startswith('From '): continue
+        words = line.split()
+        days = words[2]
+        if days not in counter:
+            counter[days] = 1
+        else:
+            counter[days] += 1
 
+    print(counter)
 
 
 ## if you want to test locally run > python payCalculator.py
